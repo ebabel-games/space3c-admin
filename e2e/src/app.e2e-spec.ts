eb680ba442935c3a2d0a94174d9b99a3eefc69d5
Thomas Amar <hello@ebabel.eu>
2020-04-18 22:24:55 +0200

    Fix end to end test.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('space3C admin');
+    expect(page.getWelcomeText()).toEqual('space3C admin app is running!');
   });
 
   afterEach(async () => {
